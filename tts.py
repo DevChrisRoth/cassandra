f9e4b7789d82326ed569222a2e77c0f7ce9d0055
Rothmann Christoph <chrisrothbog@gmail.com>
2025-02-23 11:34:51 +0100

    doc: removed comment in tts.py

--- tts.py
+++ tts.py
@@ -7,7 +7,6 @@ engine.setProperty("rate", 175)  # Geschwindigkeit der Sprachausgabe (Standard: 
 
 # Text-to-Speech Ausgabe
 def speak(text: str) -> None:
-    # Normalizer currently not working as expected
     text = normalizer.normalize_text(text)
     engine.say(text)
     engine.runAndWait()
